0bfbde1d7eed2e607dcdfcc5e6ed00059acca43e
Logan Major <lmajor@accolade-group.com>
2021-11-19 16:23:54 -0500

    updated

--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 )
 
 require (
+	github.com/denisenkom/go-mssqldb v0.11.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"database/sql"
+
+	_ "github.com/denisenkom/go-mssqldb"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	gomail "gopkg.in/mail.v2"
 )
+
+var db *sql.DB
+var err error
 
 func main() {
 	fmt.Println("blibli")
@@ -35,25 +44,50 @@ func hello(c echo.Context) error {
 
 func paramsFunc(c echo.Context) error {
 	name := c.Param("name")
-	fmt.Println(name)
-	emailerIP := os.Getenv("EMAILERIP")
-	emailer := os.Getenv("EMAILER")
+	if len(name) == 4 {
+		emailerIP := os.Getenv("EMAILERIP")
+		emailer := os.Getenv("EMAILER")
+		databaseConn := os.Getenv("DB")
+		db, err = sql.Open("sqlserver", fmt.Sprintf("sqlserver://%s", databaseConn))
+		if err != nil {
+			log.Println(err)
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+		fmt.Println("\n Connected!")
+		db.SetConnMaxLifetime(10 * time.Minute)
+		db.SetConnMaxIdleTime(15 * time.Second)
 
-	// get/post/whatever we want to do in a db, with name var here.
+		ctx := context.Background()
+		err2 := db.PingContext(ctx)
+		if err2 != nil {
+			log.Fatal("Error pinging db: " + err2.Error())
+		}
 
-	m := gomail.NewMessage()
-	m.SetHeader("From", emailer)
-	m.SetHeader("To", "lmajor@levelwear.com")
-	m.SetHeader("Subejct", "bliblibli")
-	m.SetBody("text/plain", name)
+		// get/post/whatever we want to do in a db, with name var here.
+		var result struct {
+			id string
+		}
+		err = db.QueryRowContext(ctx, `SELECT a.id
+			FROM ORDER_HDR a
+			WHERE a.numb = @name`, sql.Named("name", name)).Scan(&result.id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("\n", "id: ", name, "id results: ", result.id)
 
-	d := gomail.NewDialer(emailerIP, 25, "", "")
-	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+		m := gomail.NewMessage()
+		m.SetHeader("From", emailer)
+		m.SetHeader("To", "lmajor@levelwear.com")
+		m.SetHeader("Subejct", "bliblibli")
+		m.SetBody("text/plain", name)
+		d := gomail.NewDialer(emailerIP, 25, "", "")
+		d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
-	if err := d.DialAndSend(m); err != nil {
-		fmt.Println(err)
-		panic(err)
+		if err := d.DialAndSend(m); err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
+
 	}
-
 	return c.String(http.StatusOK, name)
 }
